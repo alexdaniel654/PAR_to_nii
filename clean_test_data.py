f2c8ecc163ef52a771f8b5177e1d4b96a5fc0c7b
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2018-11-12 11:34:36 +0000

    Made script to clear test_data folder of converted outputs

---
+++ clean_test_data.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created: 08/11/2018
+Last update: 09/11/2018
+Alex Daniel, Sir Peter Mansfield Imaging Centre, The University of Nottingham, 2018.
+
+Removes all the converted files from the test data folder i.e. gets rid of the .nii.gz outputs from pton
+"""
+
+import os
+import glob
+
+files = glob.glob('./test_data/*.nii.gz')
+
+for file in files:
+    print('Removing ' + file)
+    os.remove(file)
+    
